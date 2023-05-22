5066c1edd6c53d1a21aba1659bf00aaa5b8c71db
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2023-05-23 00:57:32 +0200

    23

--- START/src/pug/components/gal/GAL.js
+++ START/src/pug/components/gal/GAL.js
@@ -5,48 +5,110 @@ export class GAL {
     this.GAL = elem;
     this.GALitems = [...this.GAL.querySelectorAll(".GAL-item-js")];
     this.GALbutton = this.GAL.nextElementSibling;
-    this.tempHeight = this.GAL.querySelector(".GAL-item-js").clientHeight;
+    this.tempHeight = this.GAL.querySelector(".GAL-item-js").scrollHeight;
+    this.numer = 6;
   }
+
+  //  ===============================
+
   open() {
-    this.GALbutton.classList.add("_is-active");
-    this.GALbutton.querySelector("span").innerHTML = "Свернуть";
-    for (let i = 0; i < this.GALitems.length; i++) {
-      if (this.GALitems[i].classList.contains("_is-hidden")) {
-        this.GALitems[i].classList.remove("_is-hidden");
-      }
-    }
-    this.GAL.animate([{ height: `${this.tempHeight}px` }, { height: "auto" }], {
-      duration: 300,
+
+
+    var ani = this.GAL.animate([{ rowGap: 0 + "px", rowGap: `30px` }], {
+      duration: 1000,
       easing: "ease-in-out",
     });
-    
-    this.GAL.style.height = "auto";
-  }
-  close() {
-    this.GALbutton.classList.remove("_is-active");
-    this.GALbutton.querySelector("span").innerHTML = "Загрузить еще";
-    this.GAL.style.height = this.tempHeight + "px";
-    for (let i = 0; i < this.GALitems.length; i++) {
-      if (i > 2) {
-        this.GALitems[i].classList.add("_is-hidden");
-        var tempHeight = this.GAL.querySelector(".GAL-item-js").clientHeight;
-      }
-    }
-  }
-  start() {
-    for (let i = 0; i < this.GALitems.length; i++) {
-      if (i > 2) {
-        this.GALitems[i].classList.add("_is-hidden");
-      }
-    }
-    this.GAL.style.height = this.tempHeight + "px";
+    const endet = () => {
+      this.GAL.style.rowGap = `30px`;
+    };
+    ani.addEventListener("finish", (e) => {
+      endet();
+    });
 
-    this.GALbutton.addEventListener("click", (e) => {
-      if (!this.GALbutton.classList.contains("_is-active")) {
-        this.open();
-      } else {
-        this.close();
+    if (this.numer === this.GALitems.length) {
+      this.GALbutton.querySelector("span").innerHTML = "Свернуть";
+      
+      this.GALbutton.querySelector("svg").style.display = 'none';
+    }
+
+    if (this.numer <= this.GALitems.length) {
+      for (let i = 0; i < this.GALitems.length; i++) {
+        if (2 < i && i < this.numer) {
+          var anim = this.GALitems[i].animate(
+            [{ height: 0 + "px", height: `${this.tempHeight}px` }],
+            {
+              duration: 1000,
+              easing: "ease-in-out",
+            }
+          );
+          const beendet = () => {
+            this.GALitems[i].style.height = `${this.tempHeight}px`;
+          };
+          anim.addEventListener("finish", (e) => {
+            beendet();
+          });
+        }
       }
+      this.numer = this.numer + 3;
+    } else {
+      this.numer = 6;
+      this.close();
+    }
+  }
+
+  //  ===============================
+
+  close() {
+    var ani = this.GAL.animate([{ rowGap: `30px`, rowGap: 0 + "px" }], {
+      duration: 1000,
+      easing: "ease-in-out",
+    });
+    const endet = () => {
+      this.GAL.style.rowGap = 0 + "px";
+    };
+    ani.addEventListener("finish", (e) => {
+      endet();
+    });
+
+    for (let i = 0; i < this.GALitems.length; i++) {
+      if (i > 2) {
+        var anim = this.GALitems[i].animate(
+          [{ height: `${this.tempHeight}px`, height: 0 + "px" }],
+          {
+            duration: 1000,
+            easing: "ease-in-out",
+          }
+        );
+        const beendet = () => {
+          this.GALbutton.classList.remove("_is-active");
+          this.GALbutton.querySelector("span").innerHTML = "Загрузить еще";
+          this.GALitems[i].style.height = 0;
+          this.GALbutton.querySelector("svg").style.display = "block";
+        };
+        anim.addEventListener("finish", (e) => {
+          beendet();
+        });
+      }
+    }
+  }
+
+  //  ===============================
+
+  start() {
+    this.close();
+    this.GALbutton.addEventListener("click", (e) => {
+
+          this.GALbutton.classList.add("_is-active");
+      // if (!this.GALbutton.classList.contains("_is-active")) {
+          setTimeout(() => {
+             this.GALbutton.classList.remove("_is-active");
+           
+          }, 1000);
+        this.open();
+      // }
+      //  else if (this.numer <= this.GALitems.length ) {
+      //   this.close();
+      // }
     });
   }
 }
