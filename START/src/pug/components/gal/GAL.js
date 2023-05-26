186bfc5d0718990c7be2e1f70df7d9693d94e53e
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2023-05-26 12:46:51 +0200

    1

--- START/src/pug/components/gal/GAL.js
+++ START/src/pug/components/gal/GAL.js
@@ -6,30 +6,25 @@ export class GAL {
     this.GALitems = [...this.GAL.querySelectorAll(".GAL-item-js")];
     this.GALbutton = this.GAL.nextElementSibling;
     this.tempHeight = this.GAL.querySelector(".GAL-item-js").scrollHeight;
-    this.numer = 2;
+    this.numer = 0;
     this.time = 300;
     this.Addtime = 300;
-    this.numerItems = 2;
+    this.numberItems = 0;
   }
 
   //  ===============================
 
   open() {
-    console.log(this.numer);
-    // if (this.GAL.classList.contains("GAL-doctors")) {
-    //  this.numer = 1;
-    // } else {
-    //  this.numer = 6;
-    // }
 
-    // if (this.numer === this.GALitems.length) {
-    //   this.GALbutton.querySelector("span").innerHTML = "Свернуть";
-    //   this.GALbutton.querySelector("svg").style.display = "none";
-    // }
+
+    if (this.numer === this.GALitems.length) {
+      this.GALbutton.querySelector("span").innerHTML = "Свернуть";
+      this.GALbutton.querySelector("svg").style.display = "none";
+    }
 
     if (this.numer <= this.GALitems.length) {
       for (let i = 0; i < this.GALitems.length; i++) {
-        if (2 < i && i < this.numer) {
+        if (this.numberItems < i && i < this.numer) {
           var animheight = this.GALitems[i].animate(
             [{ height: 0 + "px", height: `${this.tempHeight}px` }],
 
@@ -59,30 +54,32 @@ export class GAL {
         }
       }
 
-      // if (this.GAL.classList.contains("GAL-doctors")) {
-      //   this.numer = this.numer + 1;
-      // } else {
-      //   this.numer = this.numer + 3;
-      // }
-    }
-    //  else {
-    //   this.numer = 3;
+      
+       if (this.GAL.classList.contains("GAL-doctors")) {
+         this.numer = this.numer + 1;
+       } else {
+         this.numer = this.numer + 3;
+       }
+    } else {
+      this.numer = 1;
 
-    //   this.close();
-    // }
+      this.close();
+    }
   }
 
   //  ===============================
 
   close() {
-    // if (this.GAL.classList.contains("GAL-doctors")) {
-    //   this.numberItems = 0;
-
-    // } else {
-    //   this.numberItems = 2;
-    // }
+   
+    if (this.GAL.classList.contains("GAL-doctors")) {
+      this.numberItems = 0;
+    
+     
+    } else {
+      this.numberItems = 2;
+    }
     for (let i = 0; i < this.GALitems.length; i++) {
-      if (i > this.numerItems) {
+      if (i > this.numberItems) {
         var anim = this.GALitems[i].animate(
           [{ height: `${this.tempHeight}px`, height: 0 + "px" }],
           {
@@ -130,21 +127,5 @@ export class GAL {
         this.Addtime = 300;
       }, this.time + this.Addtime);
     });
-  }
-
-  startGal() {
-    this.close();
-
-    // this.GALbutton.addEventListener("click", (e) => {
-    //        this.GALbutton.classList.add("_is-active");
-    //        if (this.numer > this.GALitems.length) {
-    //          this.Addtime = -300;
-    //        }
-    //          setTimeout(() => {
-    //            this.GALbutton.classList.remove("_is-active");
-    //            this.open();
-    //            this.Addtime = 300;
-    //          }, this.time + this.Addtime);
-    // });
   }
 }
