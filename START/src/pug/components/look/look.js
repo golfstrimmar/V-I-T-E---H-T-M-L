7c8c82573fca0aa1c8a88827702380ffc533a635
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2023-07-27 15:12:22 +0200

    44

--- START/src/pug/components/look/look.js
+++ START/src/pug/components/look/look.js
@@ -4,6 +4,10 @@ import Plyr from "plyr";
 export const Look = () => {
   if (document.querySelector("#player-1")) {
     const r = document.querySelector("#player-1");
-    const simplePlir = new Plyr(r);
+    const simplePlir1 = new Plyr(r);
+  }
+  if (document.querySelector("#player-2")) {
+    const r = document.querySelector("#player-2");
+    const simplePlir2 = new Plyr(r);
   }
 };
